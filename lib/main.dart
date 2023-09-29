632293e163dd3c8b44bc90ea34f2074e69d20932
Mohamed3li454 <mohhamed4413@gmail.com>
2023-09-29 09:17:51 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/views/Home_view.dart';
+import 'package:news_app/views/home_view.dart';
 
 void main() {
   runApp(const News_cloud());
